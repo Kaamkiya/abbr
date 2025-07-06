7d793c163d8454a91e6344a91e0e52babf0dcfbc
Kaamkiya <ziam3.1415@gmail.com>
2025-07-06 09:49:22 -0400

    feat: Add web interface

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/Kaamkiya/nanoid-go"
@@ -10,18 +11,26 @@ var pairs = map[string]string{}
 
 func main() {
 	http.HandleFunc("/create", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r)
+
 		if r.Pattern != "/create" {
 			w.WriteHeader(http.StatusNotFound)
 			return
 		}
 
-		if r.Method != "POST" {
+		if r.Method != "POST" && r.Method != "GET" {
 			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		if r.Method == "GET" {
+			http.ServeFile(w, r, "create.html")
 			return
 		}
 
 		r.ParseForm()
 		to := r.Form.Get("to")
+		html := r.Form.Get("webform") == "true"
 		add := true
 		key := nanoid.Default()
 
@@ -35,10 +44,18 @@ func main() {
 		if add {
 			pairs[key] = to
 		}
+
+		if html {
+			w.Write([]byte(`<!DOCTYPE html><p>Short URL: <a href="/` + key + `">` + key + `</a></p>`))
+			return
+		}
+
 		w.Write([]byte(key))
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r)
+
 		path := r.URL.Path
 		if path[0] == '/' {
 			path = path[1:]
